4637fe475820e71c07424eaec993009e3543edfc
Devanginisingh2002 <84927553+Devanginisingh2002@users.noreply.github.com>
2022-02-01 23:00:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+
+
+devices.forEach(function(device) {
+  $('#devices tbody').append(`
+    <tr>
+      <td>${device.user}</td>
+      <td>${device.name}</td>
+      <td>${device.email}</td>
+    </tr>`
+  );
+});
+
+
+
+$('#add-device').on('click', function() {
+  const user = $('#user').val();
+  const name = $('#name').val();
+  const email = $('#email').val();
+  devices.push({ user, name, email });
+  localStorage.setItem('devices', JSON.stringify(devices));
+  location.href = 'device-list.html';
+});
